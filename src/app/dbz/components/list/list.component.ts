f25c2e1a202b3495b2eb12c2dcdc5d51263cc075
ecabrera938 <ecabrera938@gmail.com>
2023-04-20 19:55:33 -0600

    Primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { outputAst } from '@angular/compiler';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent {
+  @Output()
+  onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  characterList: Character[] = [
+    {
+      id: '',
+      name: '',
+      power: 0,
+    },
+  ];
+
+  onDeleteCharacter(id?: string): void {
+    console.log(id);
+    this.onDelete.emit(id);
+  }
+}
